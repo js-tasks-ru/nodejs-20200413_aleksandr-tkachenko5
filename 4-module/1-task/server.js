8ed23a34e75c86031b86654006fb4eac492bbae2
Aleksandr <aleksandr.tkachenko@runsel.com>
2020-04-27 19:31:40 +0300

    Add solution '4-module/1-task'

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,16 +12,7 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'GET':
-      /**
-       * Check for the nested path.
-       * If so, response with the "400" status code
-       * */
-      if (pathname.includes('/')) {
-        res.statusCode = 400;
-        res.end();
-      }
-      console.log('d');
-      res.end();
+      sendFile(pathname, filepath, res);
       break;
 
     default:
@@ -28,5 +20,45 @@ server.on('request', (req, res) => {
       res.end('Not implemented');
   }
 });
+
+function sendFile(pathname, filepath, res) {
+  /**
+   * Create a stream and pipe it to the respone.
+   * */
+  const file = fs.createReadStream(filepath);
+  file.pipe(res);
+
+  file.on('error', (err) => {
+    if (pathname.includes('/')) {
+      /**
+       * Check for the nested path.
+       * If so, response with the "400" status code.
+       * */
+      res.statusCode = 400;
+      res.end('Nested path does not supported');
+    } else if (!fs.stat(filepath, (err) => err)) {
+      /**
+       * Check the file existence.
+       * If the file does not exist, response with the "404" status code.
+       * */
+      res.statusCode = 404;
+      res.end('No such file');
+    } else {
+      /**
+       * Unknown server error.
+       * Response with the "500" status code.
+       * */
+      res.statusCode = 500;
+      res.end('Server error');
+    }
+  });
+  /**
+   * If connection was closed,
+   * destroy all resources associated with the stream.
+   * */
+  res.on('close', () => {
+    file.destroy();
+  });
+}
 
 module.exports = server;
