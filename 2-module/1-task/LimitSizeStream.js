99a7f72552075005cba4543f475dec1ca82cde22
Aleksandr <aleksandr.tkachenko@runsel.com>
2020-04-28 20:39:36 +0300

    Fix '2-module/1-task'

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -4,17 +4,20 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
     this.limit = options.limit;
     this.bufferSize = 0;
   }
 
   _transform(chunk, encoding, callback) {
     this.bufferSize += Buffer.from(chunk).length;
+
     if (this.bufferSize > this.limit) {
       callback(new LimitExceededError());
+    } else {
+      this.push(chunk.toString());
+      callback();
     }
-    this.push(chunk.toString());
-    callback();
   }
 }
 
