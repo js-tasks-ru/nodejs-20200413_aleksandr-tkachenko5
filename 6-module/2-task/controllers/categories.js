ef5bcb639762c06795bfbd7339fa029f1d9c77ae
Aleksandr <aleksandr.tkachenko@runsel.com>
2020-05-03 17:04:39 +0300

    Add categories list

--- 6-module/2-task/controllers/categories.js
+++ 6-module/2-task/controllers/categories.js
@@ -1,3 +1,8 @@
+const Category = require('../models/Category');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const document = await Category.find({});
+
+  ctx.body = {categories: document};
+  return next();
 };
