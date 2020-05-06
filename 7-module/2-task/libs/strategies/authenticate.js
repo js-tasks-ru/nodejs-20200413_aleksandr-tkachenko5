3c2d6a5659f44ab30941af9a0ea793f2259102ca
Aleksandr <aleksandr.tkachenko@runsel.com>
2020-05-06 22:58:13 +0300

    Add '7-module/2-task' solution

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,3 +1,37 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  /**
+   * Callback: done(err, data, info)
+   * err - error that not related to the logic (for example server error).
+   * data - data that passed, it can be user data (or false, if some of checks failed).
+   * info - optional additional info, for example if some checks failed.
+   * */
+
+  try {
+    /**
+     * Check if the function is receive the user email.
+     * */
+    if (!email) {
+      return done(null, false, 'Не указан email');
+    }
+
+    /**
+     * Find the user by the passed email.
+     * */
+    const user = await User.findOne({email});
+
+    /**
+     * If the user does not exist, create it, save it, pass it.
+     * */
+    if (!user) {
+      const user = new User({email, displayName});
+      await user.save();
+      return done(null, user);
+    }
+
+    done(null, user);
+  } catch (e) {
+    done(e);
+  }
 };
