7839a2099182c995eab1a534fbe6e73261475770
javascript-ru <47786167+jsru@users.noreply.github.com>
2020-05-12 18:40:39 +0200

    Update 7-module-3-task

--- 7-module/3-task/libs/strategies/local.js
+++ 7-module/3-task/libs/strategies/local.js
@@ -4,12 +4,21 @@ const User = require('../../models/User');
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
     async function(email, password, done) {
-      const user = await User.login(email, password);
+      try {
+        const user = await User.findOne({email});
+        if (!user) {
+          return done(null, false, 'Нет такого пользователя');
+        }
 
-      if (user instanceof User) {
+        const isValidPassword = await user.checkPassword(password);
+
+        if (!isValidPassword) {
+          return done(null, false, 'Неверный пароль');
+        }
+
         return done(null, user);
-      } else {
-        return done(null, false, user);
+      } catch (err) {
+        done(err);
       }
     }
 );
