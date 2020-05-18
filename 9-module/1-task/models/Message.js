f3a7c33377caf9c1663f9a8c9cbf829216102acf
Aleksandr <aleksandr.tkachenko@runsel.com>
2020-05-18 18:12:45 +0300

    Add '9-module/1-task' solution

--- 9-module/1-task/models/Message.js
+++ 9-module/1-task/models/Message.js
@@ -24,4 +24,23 @@ const messageSchema = new mongoose.Schema({
 
 });
 
+/**
+ * Get the formatted history of the messages.
+ * */
+messageSchema.statics.getFormattedMessageList = async function() {
+  /**
+   * For the testing purposes we limit it to 1.
+   * Should be ~20.
+   * */
+  const messageList = await this.find({}).limit(1);
+  return messageList.map((item) => {
+    return {
+      date: item.date,
+      id: item._id,
+      text: item.text,
+      user: item.user,
+    };
+  });
+};
+
 module.exports = connection.model('Message', messageSchema);
